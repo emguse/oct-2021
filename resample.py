eff31178c27edd779020ca1e443566532fa2a0d8
emguse <30695541+emguse@users.noreply.github.com>
2021-10-12 22:24:41 +0900

    Modifications are being made to handle multiple chunks.

--- resample.py
+++ resample.py
@@ -12,7 +12,7 @@ data = pd.read_csv(FILE_NAME).values.tolist()
 # Create a deque object and make it a queue
 d = deque(data)
 
-# Find the number of chunks
+# Find number of chunks
 start_time = d[0][0]
 last_time = d[len(d)-1][0]
 total_time = last_time - start_time
@@ -23,33 +23,59 @@ if chunk_count[1] == 0:
 else:
     chunk_count = int(chunk_count[0]) + 1
 print("chunk_count: "+ str(chunk_count))
-# 1234567890
-# 1234 -> 123 
-# #For the first chunk, use the first 3/4.
-# 3456 -> 45 -> 12345 
-# For the second and subsequent chunks, 
-# use the starting position 3/4 of the way down, 
-# discard the first 1/4 and the last 1/4, and use the middle 2/4.
-# ...
-# 7890 -> 890 -> 1234567890
-# For the last chunk, use the last 3/4.
 
+for i in range(chunk_count):
+    if i == chunk_count-1: # Zero-filling of the last chunk
+        last_chunk_head = d[0][0]
+        while last_chunk_head + CHUNK_SIZE >= d[len(d)-1][0]:
+            last_time = d[len(d)-1][0]
+            fill_zero = [last_time+1, 0]
+            d.append(fill_zero)
+    # Get seconds of data as a single chunk
+    start_row = d[0][0]
+    chunk = []
+    while start_row+CHUNK_SIZE >= d[0][0]:
+        chunk.append(d.popleft())
+    print(len(chunk))
 
-# Get same seconds of data as a single chunk
-start_row = d[0][0]
-chunk = []
-while start_row+CHUNK_SIZE >= d[0][0]:
-    chunk.append(d.popleft())
-print(len(chunk))
+    # resample
+    df = pd.DataFrame(chunk)
+    df = df.set_axis(['time', 'pressure'], axis=1)
+    df = df.set_index('time')
+    f = interpolate.interp1d(df.index, df.pressure, kind='cubic')
+    re_x = np.arange(df.index.min(), df.index.max(), 1/Hz) # 後で直す
+    re_y = f(re_x)
 
-# resample
-df = pd.DataFrame(chunk)
-df = df.set_axis(['time', 'pressure'], axis=1)
-df = df.set_index('time')
-f = interpolate.interp1d(df.index, df.pressure, kind='cubic')
-re_x = np.arange(df.index.min(), df.index.max(), 1/Hz)
-re_y = f(re_x)
-re_df = pd.DataFrame(re_y, index=[re_x])
+    #re_df = pd.DataFrame(re_y, re_x)
+    #print(re_df)
+    #re_xy = re_df.to_numpy()
+    #print(re_xy)
+
+    store_df = pd.DataFrame()
+
+    if i == 0:    # Store the first chunk
+        #first_x = np.delete(re_x, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
+        #first_y = np.delete(re_y, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
+        #first_df = pd.DataFrame(first_x, first_y)
+        first_df = pd.DataFrame(re_x, re_y)
+        store_df = pd.concat([store_df, first_df], axis=0)
+    elif i == chunk_count-1:  # Store the last chunk
+        #last_x = np.delete(re_x, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
+        #last_y = np.delete(re_y, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
+        #last_df = pd.DataFrame(last_x, last_y)
+        #print(last_df)
+        last_df = pd.DataFrame(re_x, re_y)
+        store_df = pd.concat([store_df, last_df], axis=0)
+    else:   # intermediate lump
+        #intermediate_X = np.delete(re_x, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
+        #intermediate_X = np.delete(intermediate_X, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
+        #intermediate_y = np.delete(re_y, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
+        #intermediate_y = np.delete(intermediate_y, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
+        #intermediate_df = pd.DataFrame(intermediate_X, intermediate_y)
+        intermediate_df = pd.DataFrame(re_x, re_y)
+        store_df = pd.concat([store_df, intermediate_df], axis=0)
+
+print(store_df)
 
 # save csv
-re_df.to_csv('reample.csv')
+#store_df.to_csv('reample.csv')
