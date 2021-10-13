c69c85c63e0a7e66dab90b6c97810f61f9ed7d06
emguse <30695541+emguse@users.noreply.github.com>
2021-10-13 22:37:39 +0900

    Fixes all chunks to be concatenated.

--- resample.py
+++ resample.py
@@ -5,7 +5,7 @@ import numpy as np
 
 Hz = 256 # resampling rate
 CHUNK_SIZE = 4 # chunk size [sec]
-FILE_NAME = "./press_log/20211009-163236.csv" #"pressure.csv"
+FILE_NAME = "./press_log/20211012-010152.csv" #"pressure.csv"
 
 data = pd.read_csv(FILE_NAME).values.tolist()
 
@@ -17,12 +17,15 @@ start_time = d[0][0]
 last_time = d[len(d)-1][0]
 total_time = last_time - start_time
 chunk_count = divmod(total_time, CHUNK_SIZE)
+org_sample_count = len(d)
 print("total_time: "+ str(total_time))
 if chunk_count[1] == 0:
     chunk_count = chunk_count[0]
 else:
     chunk_count = int(chunk_count[0]) + 1
 print("chunk_count: "+ str(chunk_count))
+
+store_df = pd.DataFrame()
 
 for i in range(chunk_count):
     if i == chunk_count-1: # Zero-filling of the last chunk
@@ -51,20 +54,18 @@ for i in range(chunk_count):
     #re_xy = re_df.to_numpy()
     #print(re_xy)
 
-    store_df = pd.DataFrame()
-
     if i == 0:    # Store the first chunk
         #first_x = np.delete(re_x, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
         #first_y = np.delete(re_y, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
         #first_df = pd.DataFrame(first_x, first_y)
-        first_df = pd.DataFrame(re_x, re_y)
+        first_df = pd.DataFrame(re_y, re_x)
         store_df = pd.concat([store_df, first_df], axis=0)
     elif i == chunk_count-1:  # Store the last chunk
         #last_x = np.delete(re_x, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
         #last_y = np.delete(re_y, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
         #last_df = pd.DataFrame(last_x, last_y)
         #print(last_df)
-        last_df = pd.DataFrame(re_x, re_y)
+        last_df = pd.DataFrame(re_y, re_x)
         store_df = pd.concat([store_df, last_df], axis=0)
     else:   # intermediate lump
         #intermediate_X = np.delete(re_x, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
@@ -72,8 +73,10 @@ for i in range(chunk_count):
         #intermediate_y = np.delete(re_y, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
         #intermediate_y = np.delete(intermediate_y, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
         #intermediate_df = pd.DataFrame(intermediate_X, intermediate_y)
-        intermediate_df = pd.DataFrame(re_x, re_y)
+        intermediate_df = pd.DataFrame(re_y, re_x)
         store_df = pd.concat([store_df, intermediate_df], axis=0)
+    
+
 
 print(store_df)
 
