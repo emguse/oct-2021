34c35a4d9cc2f2c5897bb3ece7973e9d1760bd5a
emguse <30695541+emguse@users.noreply.github.com>
2021-10-14 00:13:40 +0900

    Delete extra data at the end

--- resample.py
+++ resample.py
@@ -17,15 +17,12 @@ start_time = d[0][0]
 last_time = d[len(d)-1][0]
 total_time = last_time - start_time
 chunk_count = divmod(total_time, CHUNK_SIZE)
-org_sample_count = len(d)
 print("total_time: "+ str(total_time))
 if chunk_count[1] == 0:
     chunk_count = chunk_count[0]
 else:
     chunk_count = int(chunk_count[0]) + 1
 print("chunk_count: "+ str(chunk_count))
-
-store_df = pd.DataFrame()
 
 for i in range(chunk_count):
     if i == chunk_count-1: # Zero-filling of the last chunk
@@ -34,7 +31,7 @@ for i in range(chunk_count):
             last_time = d[len(d)-1][0]
             fill_zero = [last_time+1, 0]
             d.append(fill_zero)
-    # Get seconds of data as a single chunk
+    # Get seconds of data as a single chunks
     start_row = d[0][0]
     chunk = []
     while start_row+CHUNK_SIZE >= d[0][0]:
@@ -49,35 +46,33 @@ for i in range(chunk_count):
     re_x = np.arange(df.index.min(), df.index.max(), 1/Hz) # 後で直す
     re_y = f(re_x)
 
-    #re_df = pd.DataFrame(re_y, re_x)
-    #print(re_df)
-    #re_xy = re_df.to_numpy()
-    #print(re_xy)
-
     if i == 0:    # Store the first chunk
         #first_x = np.delete(re_x, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
         #first_y = np.delete(re_y, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
         #first_df = pd.DataFrame(first_x, first_y)
-        first_df = pd.DataFrame(re_y, re_x)
-        store_df = pd.concat([store_df, first_df], axis=0)
+        first_a = np.stack([re_x, re_y],1)
+        store_a = first_a
+
     elif i == chunk_count-1:  # Store the last chunk
         #last_x = np.delete(re_x, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
         #last_y = np.delete(re_y, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
         #last_df = pd.DataFrame(last_x, last_y)
         #print(last_df)
-        last_df = pd.DataFrame(re_y, re_x)
-        store_df = pd.concat([store_df, last_df], axis=0)
+        last_a = np.stack([re_x, re_y],1)
+        store_a = np.concatenate([store_a, last_a])
+
     else:   # intermediate lump
         #intermediate_X = np.delete(re_x, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
         #intermediate_X = np.delete(intermediate_X, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
         #intermediate_y = np.delete(re_y, slice(None, int(Hz*CHUNK_SIZE*1/4)), None)
         #intermediate_y = np.delete(intermediate_y, slice(int(Hz*CHUNK_SIZE*3/4), None), None)
         #intermediate_df = pd.DataFrame(intermediate_X, intermediate_y)
-        intermediate_df = pd.DataFrame(re_y, re_x)
-        store_df = pd.concat([store_df, intermediate_df], axis=0)
-    
+        intermediate_a = np.stack([re_x, re_y],1)
+        store_a = np.concatenate([store_a, intermediate_a])
 
-
+store_a = np.delete(store_a, slice(int(total_time*Hz), None) , 0)
+print(int(total_time*Hz))
+store_df = pd.DataFrame(store_a)
 print(store_df)
 
 # save csv
