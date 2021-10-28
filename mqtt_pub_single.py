8618f035e03d2a7a8c30124b93c682e267405114
emguse <30695541+emguse@users.noreply.github.com>
2021-10-28 21:38:16 +0900

    mqtt single pub Classified

--- mqtt_pub_single.py
+++ mqtt_pub_single.py
@@ -1,11 +1,26 @@
 import paho.mqtt.publish as publish
 import json
 
-broker = 'host'
-port = 1883
-topic = "rspi/env"
+BROKER = 'hanage'
+PORT = 1883
+TOPIC = "rspi/env"
 # username = 'user'
 # password = 'pass'
-data = {'tmp': 23.0, 'hum': 54.0, 'atm':998.0}
-msg = json.dumps(data)
-publish.single(topic, msg, hostname=broker, port=port)
+
+class mqtt_pub_single():
+    def __init__(self, topic=TOPIC, broker=BROKER, port=PORT) -> None:
+        self.topic = topic
+        self.broker = broker
+        self.port = port
+        pass
+    def pub_single(self, data):
+        msg = json.dumps(data)
+        publish.single(self.topic, msg, hostname=self.broker, port=self.port)
+
+def main():
+    data = {'tmp': 23.0, 'hum': 54.0, 'atm':998.0}
+    msg = json.dumps(data)
+    publish.single(TOPIC, msg, hostname=BROKER, port=PORT)
+
+if __name__ == '__main__':
+    main()
